d568e5109c4af87ad0178a1f2d0f68665e0d3981
Ghanshyam Bambale <gbambale@avaya.com>
2021-12-06 18:03:54 +0530

    ArrayList example added

--- src/StringClassEx.java
+++ src/StringClassEx.java
@@ -7,11 +7,15 @@ public class StringClassEx {
 	      String str3 = new String("World");
 	      
 	      int result = str1.compareTo( str2 );
-	      System.out.println(result);
+	      System.out.println("Comparison result of 2 strings Hello and Hello is = "+result);
 	      
 	      result = str2.compareTo( str3 );
-	      System.out.println(result);
+	      System.out.println("Comparison result of 2 strings Hello and World is = "+result);
 	      
 	      System.out.println(str1.compareTo("Strings are not"));
+	      
+	      // Equals
+	      boolean result_bool = str1.equals(str2);
+	      System.out.println("Equals result of 2 strings Hello and Hello is = "+result_bool);
 	   }
 	}
