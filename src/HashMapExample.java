cf8b94d9e44d59b806745f6841dad69e44197bf2
Ghanshyam Bambale <gbambale@avaya.com>
2021-12-07 07:11:25 +0530

    some functions of hashmap implemented

--- src/HashMapExample.java
+++ src/HashMapExample.java
@@ -33,6 +33,9 @@ public class HashMapExample {
 		HashMap<String, String> someOtherNewHash = new HashMap<String, String>();
 		someOtherNewHash.put("name", "Jil");
 		someOtherNewHash.put("age", "20");
+		someOtherNewHash.put("Notage", "20");
+		System.out.println("Item 1 age:"+someOtherNewHash.get("age"));
+		System.out.println("Item 1 Notage:"+someOtherNewHash.get("Notage"));
 		// compare 2 hashmaps
 		if(someOtherHash.equals(someOtherNewHash))
 		{
@@ -49,6 +52,18 @@ public class HashMapExample {
 		 * OPERATIONS on Hashmap
 		 * Iterator iterHash = someHash.entrySet().iterator();
 		 * In the iterator creation you can see that type of iterator is Map.Entry and you need to specify which kind of entry is it to avoid warnings
+		 * MERGE - Merges 2 hashmaps and maintains single copy of duplicates
+		 * isEmpty - to check if hashmap is empty
+		 * replace(key,value) - updates record with new value
+		 * clone - clones the hashmap to create new one
+		 * keyset() and valueset() to get respective sets
+		 * putIfAbsent(key,val)
+		 * remove(key) and remove(key,val)
+		 * containsKey - checks if key is present in hashmap
+		 * containsValue = checks if the value is present in hashmap
+		 * equals - compare 2 hash for equality
+		 * size - retruns size of the hashmap
+		 * 
 		 */
 		Iterator<Map.Entry<String,String>> iterHash = someHash.entrySet().iterator();
 		while(iterHash.hasNext())
@@ -56,5 +71,19 @@ public class HashMapExample {
 			Map.Entry<String,String> someMap = (Map.Entry<String, String>) iterHash.next();
 			System.out.println("Object received: key= "+someMap.getKey() + " and value= "+someMap.getValue());
 		}
+		
+		// isEmpty
+		System.out.println("Is original hashmap empty:"+someHash.isEmpty());
+		
+		// clone - creates shallow copy
+		HashMap<String, String> cloneHash = (HashMap<String, String>) someOtherNewHash.clone();
+		cloneHash.put("Notage","30");
+		System.out.println("Updated value for Notage from 20 to 30 from cloned Hashmap");
+		System.out.println("Now from original hashmap Notage:"+someOtherNewHash.get("Notage"));
+		System.out.println("Clone hashmap value for Notage:"+cloneHash.get("Notage"));
+		cloneHash.replace("notInKeys", "35");   // doesn't produce an error and does nothing
+		System.out.println("Clone hashmap value for Notage after replace:"+cloneHash.get("Notage"));
+		System.out.println("Size of the hashmap: "+cloneHash.size());
+		
 	}
 }
